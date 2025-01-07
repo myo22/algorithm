986a171c232f80e5e133232e09873879661822c5
myo22 <dlalsgud12@naver.com>
2025-01-07 17:55:21 +0900

    [1730] 판화

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,51 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int T = in.nextInt();
-        while (T-- > 0) {
-            int H = in.nextInt();
-            int W = in.nextInt();
-            int N = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] board = new int[N][N];
+        char[] chars = sc.next().toCharArray();
 
-            int distance = (N - 1) / H + 1;
-            int floor = (N - 1) % H + 1;
-            System.out.printf("%d%02d\n",floor, distance);
+        int vertical  = 0;
+        int horizon = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case 'U':
+                    board[horizon][vertical] |= 2;
+                    board[horizon][--vertical] |= 2;
+                    break;
+                case 'D':
+                    board[horizon][vertical] |= 2;
+                    board[horizon][++vertical] |= 2;
+                    break;
+                case 'L':
+                    board[horizon][vertical] |= 1;
+                    board[--horizon][vertical] |= 1;
+                    break;
+                case 'R':
+                    board[horizon][vertical] |= 1;
+                    board[++horizon][vertical] |= 1;
+                    break;
+            }
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                char outputChar;
+                if (board[j][i] == 3) {
+                    outputChar = '+';
+                } else if (board[j][i] == 2) {
+                    outputChar = '|';
+                } else if (board[j][i] == 1) {
+                    outputChar = '-';
+                } else {
+                    outputChar = '.';
+                }
+                System.out.print(outputChar);
+            }
+            System.out.println();
         }
     }
 }
