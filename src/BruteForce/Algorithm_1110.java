7f01e89dcf542fbfe7bffcad9d9a8227e35181ec
myo22 <dlalsgud12@naver.com>
2025-01-30 16:32:23 +0900

    [1110] 더하기 사이클

--- src/BruteForce/Algorithm_1110.java
+++ src/BruteForce/Algorithm_1110.java
@@ -1,6 +1,8 @@
-import java.util.*;
+package BruteForce;
 
-class Main {
+import java.util.Scanner;
+
+public class Algorithm_1110 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
