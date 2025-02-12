81c3950275b6acd2d5dc39de1dfe528e067e8492
myo22 <dlalsgud12@naver.com>
2025-02-12 19:41:57 +0900

    [1181] 단어 정렬

--- src/Sort/Algorithm_1181.java
+++ src/Sort/Algorithm_1181.java
@@ -1,18 +1,19 @@
-import java.util.*;
+package Sort;
 
-class Main {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
+public class Algorithm_1181 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        Set<String> word = new HashSet<>();
+        String[] words = new String[N];
 
         for(int i = 0; i < N; i++) {
-            word.add(sc.next());
+            words[i] = sc.next();
         }
-
-        String[] words =  word.toArray(new String[0]);
 
         Arrays.sort(words, new Comparator<String>() {
             @Override
@@ -24,7 +25,7 @@ class Main {
             }
         });
 
-        for(int i = 0; i < words.length; i++) {
+        for(int i = 0; i < N; i++) {
             System.out.println(words[i]);
         }
     }
