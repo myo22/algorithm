4e67f7ba00c4fae0d57e5a8b1e4c8328ce459044
myo22 <dlalsgud12@naver.com>
2025-02-17 19:35:52 +0900

    [7785] 회사에 있는 사람

--- src/Sort/Algorithm_7785.java
+++ src/Sort/Algorithm_7785.java
@@ -4,59 +4,87 @@ import java.io.*;
 import java.util.*;
 
 public class Algorithm_7785 {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int n = Integer.parseInt(br.readLine());
-//
-//        Map<String, String> map = new HashMap<>();
-//
-//        for(int i = 0; i < n; i++) {
-//            String[] arr = br.readLine().split(" ");
-//            map.put(arr[0], arr[1]);
-//        }
-//
-//        List<String> list = new ArrayList<>(map.keySet());
-//
-//        list.removeIf(answer -> map.get(answer).equals("leave"));
-//
-//        Collections.sort(list, Collections.reverseOrder());
-//
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//
-//        for(String answer : list) {
-//            bw.write(answer);
-//        }
-//
-//        bw.flush();
-//    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        String[][] records = new String[n][2];
+        Map<String, String> map = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             String[] arr = br.readLine().split(" ");
-            records[i][0] = arr[0];
-            records[i][1] = arr[1];
+            map.put(arr[0], arr[1]);
         }
 
-        Arrays.sort(records, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                return o2[0].compareTo(o1[0]);
-            }
-        });
+        List<String> list = new ArrayList<>(map.keySet());
 
-        for (int i = 0; i < n - 1; i++) {
-            if (records[i][1].equals("enter") && !records[i][0].equals(records[i + 1][0])) {
-                System.out.println(records[i][0]);
-            }
+        list.removeIf(answer -> map.get(answer).equals("leave"));
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (String answer : list) {
+            bw.write(answer);
         }
 
-        if (records[n - 1][1].equals("enter")) {
-            System.out.println(records[n - 1][0]);
-        }
-
+        bw.flush();
     }
+
+    // 2.  Sort를 활용한 방법
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//
+//        String[][] records = new String[n][2];
+//        for (int i = 0; i < n; i++) {
+//            String[] arr = br.readLine().split(" ");
+//            records[i][0] = arr[0];
+//            records[i][1] = arr[1];
+//        }
+//
+//        Arrays.sort(records, new Comparator<String[]>() {
+//            @Override
+//            public int compare(String[] o1, String[] o2) {
+//                return o2[0].compareTo(o1[0]);
+//            }
+//        });
+//
+//        for (int i = 0; i < n - 1; i++) {
+//            if (records[i][1].equals("enter") && !records[i][0].equals(records[i + 1][0])) {
+//                System.out.println(records[i][0]);
+//            }
+//        }
+//
+//        if (records[n - 1][1].equals("enter")) {
+//            System.out.println(records[n - 1][0]);
+//        }
+//
+//    }
+
+    // 3. Set을 이용한 방법
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//
+//        Set<String> entered = new TreeSet<>();
+//        for (int i = 0; i < n; i++) {
+//            String[] arr = br.readLine().split(" ");
+//            String name = arr[0];
+//            String state = arr[1];
+//            if(state.equals("enter")) {
+//                entered.add(name);
+//            }
+//            else{
+//                entered.remove(name);
+//            }
+//        }
+//
+//        String[] orderedAnswer = entered.toArray(new String[entered.size()]);
+//        for(int i = orderedAnswer.length - 1; i >= 0; i--){
+//            System.out.println(orderedAnswer[i]);
+//        }
+//
+//    }
+//}
+
 }
