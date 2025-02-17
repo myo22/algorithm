7377c9a850343755aca864a92a103cc01b4a3e4c
myo22 <dlalsgud12@naver.com>
2025-02-17 17:56:42 +0900

    [7785] 회사에 있는 사람

--- src/Sort/Algorithm_7785.java
+++ src/Sort/Algorithm_7785.java
@@ -1,7 +1,9 @@
+package Sort;
+
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Algorithm_7785 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -27,6 +29,4 @@ class Main {
 
         bw.flush();
     }
-
-
 }
