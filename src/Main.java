f4f58516d1127d9bb402a7d848759d40ca2b7c24
myo22 <dlalsgud12@naver.com>
2025-02-21 17:57:21 +0900

    [18870] 좌표 압축

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,39 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    // Sort로만 풀기
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        Map<String, Integer> map = new HashMap<>();
-
+        int[][] coordinate = new int[N][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            String title = sc.next();
-            map.put(title, map.getOrDefault(title, 0) + 1);
+            int x = Integer.parseInt(st.nextToken());
+            coordinate[i][0] = x;
+            coordinate[i][1] = i;
         }
 
-        List<String> list = new LinkedList<>(map.keySet());
-
-        list.sort(new Comparator<String>() {
+        Arrays.sort(coordinate, new Comparator<int[]>() {
             @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
             }
         });
 
-        int maxValue = 0;
-        String answer = "";
-
-        for(String title :list) {
-            if(map.get(title) > maxValue) {
-                maxValue = map.get(title);
-                answer = title;
+        int idx = 0;
+        int[] answer = new int[N];
+        answer[coordinate[0][1]] = idx;
+        for(int i = 1; i < N; i++) {
+            if(coordinate[i - 1][0] != coordinate[i][0]){
+                idx++;
             }
+            answer[coordinate[i][1]] = idx;
         }
 
-        System.out.println(answer);
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++)
+            bw.write(answer[i] + " ");
+        bw.flush();
     }
 }
---
+++ src/Sort/Algorithm_18870.java
@@ -0,0 +1,39 @@
+package Sort;
+
+import java.util.*;
+import java.io.*;
+
+public class Algorithm_18870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] originCoordinate = new int[N];
+        int[] coordinate = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            coordinate[i] = x;
+            originCoordinate[i] = x;
+        }
+
+        Arrays.sort(coordinate);
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int idx = 0;
+        for(int i = 1; i < N; i++) {
+            map.put(coordinate[i-1], idx);
+            if(coordinate[i - 1] != coordinate[i]){
+                idx++;
+            }
+        }
+
+        for(int x : originCoordinate) {
+            System.out.print(map.get(x) + " ");
+        }
+    }
+
+
+}
