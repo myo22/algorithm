0f2e0a672a0c0e9dcd8742faf3154430b4ce7bc0
myo22 <dlalsgud12@naver.com>
2025-02-26 17:42:13 +0900

    [2910] 빈도 정렬

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,6 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static class Frequency {
-        int num;
-        int count;
-        int firstIndex;
-
-        Frequency(int num, int count, int firstIndex) {
-            this.num = num;
-            this.count = count;
-            this.firstIndex = firstIndex;
-        }
-    }
-
-    static class Message {
-        int num;
-        int idx;
-
-        Message (int num, int idx) {
-            this.num = num;
-            this.idx = idx;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,38 +9,21 @@ class Main {
         int C = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        Message[] messages = new Message[N];
+        Map<Integer, Integer> messages = new LinkedHashMap<>();
         for(int i = 0; i < N; i++) {
-            messages[i] = new Message(Integer.parseInt(st.nextToken()), i);
+            int message =  Integer.parseInt(st.nextToken());
+            messages.put(message, messages.getOrDefault(message, 0) + 1);
         }
 
+        Integer[] frequencies = messages.keySet().toArray(new Integer [messages.size()]);
+        Arrays.sort(frequencies, (o1, o2) -> messages.get(o2) - messages.get(o1));
 
-        Arrays.sort(messages, (o1, o2) -> o1.num - o2.num);
-
-        Frequency[] frequencies = new Frequency[N];
-        int frequencyIndex = 0;
-        frequencies[0] = new Frequency(messages[0].num, 1, messages[0].idx);
-        for(int i = 1; i < messages.length; i++){
-            if(messages[i].num != messages[i - 1].num) {
-                frequencies[++frequencyIndex] = new Frequency(messages[i].num, 0, messages[i].idx);
-            }
-            frequencies[frequencyIndex].count++;
-        }
-
-        Arrays.sort(frequencies , 0 , frequencyIndex + 1, new Comparator<Frequency>() {
-            @Override
-            public int compare(Frequency o1, Frequency o2){
-                if(o1.count != o2.count) {
-                    return o2.count - o1.count;
-                }
-                return o1.firstIndex - o2.firstIndex;
-            }
-        });
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i <= frequencyIndex; i++) {
-            while(frequencies[i].count-- > 0){
-                bw.write(frequencies[i].num + " ");
+        for(Integer frequency: frequencies) {
+            int count = messages.get(frequency);
+            while(count-- > 0){
+                bw.write(frequency + " ");
             }
         }
         bw.flush();
--- src/Sort/Algorithm_2910.java
+++ src/Sort/Algorithm_2910.java
@@ -69,4 +69,32 @@ public class Algorithm_2910 {
         }
         bw.flush();
     }
+
+//    // Map으로 풀기
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int C = Integer.parseInt(st.nextToken());
+//
+//        st = new StringTokenizer(br.readLine());
+//        Map<Integer, Integer> messages = new LinkedHashMap<>();
+//        for(int i = 0; i < N; i++) {
+//            int message =  Integer.parseInt(st.nextToken());
+//            messages.put(message, messages.getOrDefault(message, 0) + 1);
+//        }
+//
+//        Integer[] frequencies = messages.keySet().toArray(new Integer [messages.size()]);
+//        Arrays.sort(frequencies, (o1, o2) -> messages.get(o2) - messages.get(o1));
+//
+//
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        for(Integer frequency: frequencies) {
+//            int count = messages.get(frequency);
+//            while(count-- > 0){
+//                bw.write(frequency + " ");
+//            }
+//        }
+//        bw.flush();
+//    }
 }
