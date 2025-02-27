6610a9f4649b01c315cd29cda55074160637b2dc
myo22 <dlalsgud12@naver.com>
2025-02-27 16:57:47 +0900

    [1931] 회의실 배정

---
+++ src/Sort/Algorithm_1931.java
@@ -0,0 +1,44 @@
+package Sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Algorithm_1931 {
+
+    static class Meeting {
+        int start;
+        int end;
+
+        Meeting (int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        Meeting[] meetings = new Meeting[N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            meetings[i] = new Meeting(start, end);
+        }
+
+        Arrays.sort(meetings, (o1 , o2) -> o1.start - o2.start);
+
+        for(int i = 0; i < N; i++) {
+            int count = 0;
+            if(meetings[i].start != meetings[i].end) {
+                meetings[i]
+            }
+            count++;
+        }
+
+    }
+}
