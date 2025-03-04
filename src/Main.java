804842aad08b20df9b90476488cae1f02003f6b1
myo22 <dlalsgud12@naver.com>
2025-03-04 18:30:11 +0900

    [1431] 시리얼 번호

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,8 @@ class Main {
             public int compare(Number o1, Number o2) {
                 if(o1.number.length() != o2.number.length()) {
                     return o1.number.length() - o2.number.length();
-                }else if(o1.number.length() == o2.number.length()) {
+                }else if(o1.number.length() == o2.number.length() &&
+                        o1.sum != 0 && o2.sum != 0) {
                     return o1.sum - o2.sum;
                 }
                 return o1.number.compareTo(o2.number);
