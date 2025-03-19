03151b587ea1f6049b5d104884fe4aa9ad6ab7b8
myo22 <dlalsgud12@naver.com>
2025-03-19 17:16:03 +0900

    [11660] 구간 합 구하기 5

--- src/PrefixSum/Algorithm_11660.java
+++ src/PrefixSum/Algorithm_11660.java
@@ -11,10 +11,10 @@ public class Algorithm_11660 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[N][N];
-        for(int i = 0; i < N; i++) {
+        int[][] graph = new int[N+1][N+1];
+        for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
+            for(int j = 1; j <= N; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -22,7 +22,7 @@ public class Algorithm_11660 {
         int[][] acc = new int[N+1][N+1];
         for(int i = 1; i <= N; i++) {
             for(int j = 1; j <= N; j++) {
-                acc[j][i] = acc[j-1][i-1] + graph[j-1][i-1];
+                acc[i][j] = acc[i][j-1] + graph[i][j];
             }
         }
 
@@ -34,7 +34,7 @@ public class Algorithm_11660 {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            for(int j = x1; j < x2; j++) {
+            for(int j = x1; j <= x2; j++) {
                 sum += acc[j][y2] - acc[j][y1-1];
             }
             bw.write(sum + "\n");
