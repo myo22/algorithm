c59584796f2372144f69934aaab96a505aeadce6
myo22 <dlalsgud12@naver.com>
2025-03-19 16:50:29 +0900

    [11660] 구간 합 구하기 5

--- src/PrefixSum/Algorithm_11660.java
+++ src/PrefixSum/Algorithm_11660.java
@@ -1,7 +1,9 @@
-import java.io.*;
-import java.util.*;
+package PrefixSum;
 
-class Main {
+import java.util.*;
+import java.io.*;
+
+public class Algorithm_11660 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
