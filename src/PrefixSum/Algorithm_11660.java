18313b7b4e538741faeb7efdf21c45c590de7d93
myo22 <dlalsgud12@naver.com>
2025-03-19 18:36:35 +0900

    [11660] 구간 합 구하기 5

--- src/PrefixSum/Algorithm_11660.java
+++ src/PrefixSum/Algorithm_11660.java
@@ -11,35 +11,72 @@ public class Algorithm_11660 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[N+1][N+1];
-        for(int i = 1; i <= N; i++) {
+        int[][] graph = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= N; j++) {
+            for (int j = 1; j <= N; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] acc = new int[N+1][N+1];
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= N; j++) {
-                acc[i][j] = acc[i][j-1] + graph[i][j];
+        int[][] acc = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                acc[i][j] = acc[i][j - 1] + acc[i - 1][j] - acc[i - 1][j - 1] + graph[i][j];
             }
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i < M; i++) {
-            int sum = 0;
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            for(int j = x1; j <= x2; j++) {
-                sum += acc[j][y2] - acc[j][y1-1];
-            }
-            bw.write(sum + "\n");
+            int answer = acc[x2][y2] - acc[x2][y1 - 1] - acc[x1 - 1][y2] + acc[x1 - 1][y1 - 1];
+            bw.write(answer + "\n");
         }
 
         bw.flush();
     }
+
+    // 내가 풀어서 성공한 방식
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+//
+//        int[][] graph = new int[N+1][N+1];
+//        for(int i = 1; i <= N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            for(int j = 1; j <= N; j++) {
+//                graph[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        int[][] acc = new int[N+1][N+1];
+//        for(int i = 1; i <= N; i++) {
+//            for(int j = 1; j <= N; j++) {
+//                acc[i][j] = acc[i][j-1] + graph[i][j];
+//            }
+//        }
+//
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        for(int i = 0; i < M; i++) {
+//            int sum = 0;
+//            st = new StringTokenizer(br.readLine());
+//            int x1 = Integer.parseInt(st.nextToken());
+//            int y1 = Integer.parseInt(st.nextToken());
+//            int x2 = Integer.parseInt(st.nextToken());
+//            int y2 = Integer.parseInt(st.nextToken());
+//            for(int j = x1; j <= x2; j++) {
+//                sum += acc[j][y2] - acc[j][y1-1];
+//            }
+//            bw.write(sum + "\n");
+//        }
+//
+//        bw.flush();
+//    }
 }
