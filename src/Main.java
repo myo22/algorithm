e0b3c52550f60d75ec0380998a2392db18662ced
myo22 <dlalsgud12@naver.com>
2025-03-21 18:55:00 +0900

    [19951] 태상이의 훈련소 생활

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,36 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+        int[] arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while(M-- > 0) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            for(int i = a; i <= b; i++) {
+                if(k >= 0) {
+                    arr[i] += k;
+                }else{
+                    arr[i] -= Math.abs(k);
+                }
             }
         }
 
-        int[][] acc = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                acc[i][j] = acc[i][j - 1] + acc[i - 1][j] - acc[i - 1][j - 1] + graph[i][j];
-            }
+        for(int i = 1; i <= N; i++) {
+            System.out.print(arr[i] + " ");
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int answer = acc[x2][y2] - acc[x2][y1 - 1] - acc[x1 - 1][y2] + acc[x1 - 1][y1 - 1];
-            bw.write(answer + "\n");
-        }
-
-        bw.flush();
     }
 }
--- src/PrefixSum/Algorithm_19951.java
+++ src/PrefixSum/Algorithm_19951.java
@@ -20,14 +20,19 @@ public class Algorithm_19951 {
         while(M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
-            int b =Integer.parseInt(st.nextToken());
-            int k =Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
             for(int i = a; i <= b; i++) {
                 if(k >= 0) {
                     arr[i] += k;
+                }else{
+                    arr[i] -= Math.abs(k);
                 }
-                arr[i] -= k;
             }
+        }
+
+        for(int i = 1; i <= N; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
