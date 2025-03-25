b492ddaa806ec9f95eea227b20b5d858d7ea6e05
myo22 <dlalsgud12@naver.com>
2025-03-25 19:15:52 +0900

    [19951] 태상이의 훈련소 생활

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,24 @@ class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int[] delta = new int[N+2];
+        int[] accDelta = new int[N+1];
         while(M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
-            for(int i = a; i <= b; i++) {
-                arr[i] += k;
-            }
+            delta[a] += k;
+            delta[b+1] -= k;
+        }
+
+        int a = delta[1];
+        for(int i = 1; i <= N; i++) {
+            accDelta[i] = accDelta[i - 1] + delta[i];
+        }
+
+        for(int i = 0; i <= N; i++) {
+            arr[i] = arr[i] + accDelta[i];
         }
 
         for(int i = 1; i <= N; i++) {
