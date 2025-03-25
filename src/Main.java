4fa36522699d01b710150b41772a54802520e344
myo22 <dlalsgud12@naver.com>
2025-03-25 19:31:46 +0900

    [19951] 태상이의 훈련소 생활

--- src/Main.java
+++ src/Main.java
@@ -9,35 +9,32 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N+1];
+        int[] arr = new int[N + 1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] delta = new int[N+2];
-        int[] accDelta = new int[N+1];
-        while(M-- > 0) {
+        // 각 지시에 따른 변화량을 기록한다.
+        int[] delta = new int[N + 2];
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
             delta[a] += k;
-            delta[b+1] -= k;
+            delta[b + 1] -= k;
         }
 
-        int a = delta[1];
-        for(int i = 1; i <= N; i++) {
+        // 각 칸부터의 변화량을 각 칸에 적용한다.
+        int[] accDelta = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             accDelta[i] = accDelta[i - 1] + delta[i];
         }
 
-        for(int i = 0; i <= N; i++) {
-            arr[i] = arr[i] + accDelta[i];
+        // 변화량이 적용된 각 칸의 높이를 출력한다.
+        for (int i = 1; i <= N; i++) {
+            System.out.print(arr[i] + accDelta[i] + " ");
         }
-
-        for(int i = 1; i <= N; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
     }
 }
--- src/PrefixSum/Algorithm_19951.java
+++ src/PrefixSum/Algorithm_19951.java
@@ -17,8 +17,8 @@ public class Algorithm_19951 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 각 지시에 따른 변화량을 기록한다.
         int[] delta = new int[N+2];
-        int[] accDelta = new int[N+1];
         while(M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -28,18 +28,15 @@ public class Algorithm_19951 {
             delta[b+1] -= k;
         }
 
-        int a = delta[1];
+        // 각 칸부터의 변화량을 각 칸에 적용한다.
+        int[] accDelta = new int[N+1];
         for(int i = 1; i <= N; i++) {
             accDelta[i] = accDelta[i - 1] + delta[i];
         }
 
-        for(int i = 0; i <= N; i++) {
-            arr[i] = arr[i] + accDelta[i];
-        }
-
+        // 변화량이 적용된 각 칸의 높이를 출력한다.
         for(int i = 1; i <= N; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + accDelta[i] + " ");
         }
-
     }
 }
