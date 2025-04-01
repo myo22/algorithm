2a1fc325afec37b3d88be0aa8f6306c0a8214aaf
myo22 <dlalsgud12@naver.com>
2025-04-01 17:42:37 +0900

    [14425] 문자열 집합

--- src/BinarySearch/Algorithm_14425.java
+++ src/BinarySearch/Algorithm_14425.java
@@ -3,20 +3,43 @@ package BinarySearch;
 import java.util.*;
 
 public class Algorithm_14425 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int M = sc.nextInt();
+//
+//        Set<String> set = new HashSet<>();
+//        while(N--> 0) {
+//            set.add(sc.next());
+//        }
+//
+//        int count = 0;
+//        while(M-- > 0) {
+//            if(set.contains(sc.next())){
+//                count++;
+//            }
+//        }
+//
+//        System.out.println(count);
+//    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        Set<String> set = new HashSet<>();
-        while(N--> 0) {
-            set.add(sc.next());
+        String[] arr = new String[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.next();
         }
 
         int count = 0;
         while(M-- > 0) {
-            if(set.contains(sc.next())){
-                count++;
+            String a = sc.next();
+            for(int i = 0; i < N; i++) {
+                if(arr[i].equals(a)){
+                    count++;
+                }
             }
         }
 
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,57 @@
 import java.util.*;
 
 class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        int M = sc.nextInt();
+//
+//        Set<String> set = new HashSet<>();
+//        while(N--> 0) {
+//            set.add(sc.next());
+//        }
+//
+//        int count = 0;
+//        while(M-- > 0) {
+//            if(set.contains(sc.next())){
+//                count++;
+//            }
+//        }
+//
+//        System.out.println(count);
+//    }
+
+    public static boolean isExist(String[] arr, String x) {
+        int l = 0, r = arr.length;
+        while(l <= r) {
+            int m = (l + r) / 2;
+            int result = arr[m].compareTo(x);
+            if(result < 0) {
+                l = m + 1;
+            }else if(result > 0) {
+                r = m - 1;
+            }
+            else return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        Set<String> set = new HashSet<>();
-        while(N--> 0) {
-            set.add(sc.next());
+        String[] arr = new String[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.next();
         }
+
+        Arrays.sort(arr);
 
         int count = 0;
         while(M-- > 0) {
-            if(set.contains(sc.next())){
+            String a = sc.next();
+            if(isExist(arr, a)){
                 count++;
             }
         }
