f92fee70923fa27105e62afc77b0d3d5875d719e
myo22 <dlalsgud12@naver.com>
2025-04-02 18:07:58 +0900

    [2295] 세 수의 합

--- src/BinarySearch/Algorithm_2295.java
+++ src/BinarySearch/Algorithm_2295.java
@@ -27,23 +27,56 @@ public class Algorithm_2295 {
             arr[i] = sc.nextInt();
         }
 
-        Set<Integer> set = new HashSet<>();
-
+        int[] sums = new int[N * (N + 1) / 2];
+        int sumIndex = 0;
         for(int i = 0; i < N; i++) {
             for(int j = i; j < N; j++) {
-                set.add(arr[i] + arr[j]);
+                sums[sumIndex++] = arr[i] + arr[j];
             }
         }
+
+        Arrays.sort(sums);
+
 
         int answer = -1;
         for (int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 int target = arr[i] - arr[j];
-                if(set.contains(target)){
+                if(isExist(sums, target)){
                     answer = Math.max(answer, arr[i]);
                 }
             }
         }
         System.out.println(answer);
     }
+
+    // Set으로 푸는 방법
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//
+//        int[] arr = new int[N];
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        Set<Integer> set = new HashSet<>();
+//
+//        for(int i = 0; i < N; i++) {
+//            for(int j = i; j < N; j++) {
+//                set.add(arr[i] + arr[j]);
+//            }
+//        }
+//
+//        int answer = -1;
+//        for (int i = 0; i < N; i++) {
+//            for(int j = 0; j < N; j++) {
+//                int target = arr[i] - arr[j];
+//                if(set.contains(target)){
+//                    answer = Math.max(answer, arr[i]);
+//                }
+//            }
+//        }
+//        System.out.println(answer);
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
 class Main {
+    public static boolean isExist(int[] arr, int k) {
+        int l = 0, r = arr.length - 1;
+        while(l <= r) {
+            int m = (l + r) / 2;
+            if(arr[m] < k) {
+                l = m + 1;
+            }else if(arr[m] > k) {
+                r = m - 1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -10,19 +25,21 @@ class Main {
             arr[i] = sc.nextInt();
         }
 
-        Set<Integer> set = new HashSet<>();
-
+        int[] sums = new int[N * (N + 1) / 2];
+        int sumIndex = 0;
         for(int i = 0; i < N; i++) {
             for(int j = i; j < N; j++) {
-                set.add(arr[i] + arr[j]);
+                sums[sumIndex++] = arr[i] + arr[j];
             }
         }
+
+        Arrays.sort(sums);
 
         int answer = -1;
         for (int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 int target = arr[i] - arr[j];
-                if(set.contains(target)){
+                if(isExist(sums, target)){
                     answer = Math.max(answer, arr[i]);
                 }
             }
