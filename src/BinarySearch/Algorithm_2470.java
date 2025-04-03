5790dc22bc931405faef28203317dd5dc74b26fd
myo22 <dlalsgud12@naver.com>
2025-04-03 16:56:02 +0900

    [2470] 두 용액

--- src/BinarySearch/Algorithm_2470.java
+++ src/BinarySearch/Algorithm_2470.java
@@ -1,14 +1,16 @@
-import java.io.*;
-import java.util.*;
+package BinarySearch;
 
-class Main {
+import java.util.*;
+import java.io.*;
+
+public class Algorithm_2470 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -16,14 +18,14 @@ class Main {
 
         int answer = Integer.MAX_VALUE;
         int a = 0, b = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
+        for(int i = 0; i < N; i++) {
+            for(int j = i + 1; j < N; j++) {
                 int property = Math.abs(arr[i] + arr[j]);
-                if (property < answer) {
-                    answer = property;
-                    a = arr[i];
-                    b = arr[j];
-                }
+                 if(property < answer) {
+                     answer = property;
+                     a = arr[i];
+                     b = arr[j];
+                 }
             }
         }
 
