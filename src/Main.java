0bde25616a3e7c8e408e02c4df3ee8767c12f74a
myo22 <dlalsgud12@naver.com>
2025-04-04 17:45:04 +0900

    [2470] 두 용액

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,29 @@ import java.util.*;
 
 class Main {
     public static int isExist(int[] arr, int i, int k) {
-        int r = i, l = arr.length - 1;
-        int m = 0;
-        while(r <= l) {
-            m = (r + l) / 2;
-            if(arr[m] + k < 0) {
-                r = m + 1;
-            }else if(arr[m] + k > 0) {
-                l = m - 1;
+        int l = i, r = arr.length - 1;
+        int closestIndex = i;
+        int minDiff = Integer.MAX_VALUE;
+
+        while(l <= r) {
+            int m = (l + r) / 2;
+            int sum = arr[m] + k;
+
+            // 최소 절댓값 비교
+            if (Math.abs(sum) < minDiff) {
+                minDiff = Math.abs(sum);
+                closestIndex = m;
+            }
+
+            if(sum < 0) {
+                l = m + 1;
+            }else if(sum > 0) {
+                r = m - 1;
             }else{
                 return m;
             }
         }
-        return m;
+        return closestIndex;
     }
 
     public static void main(String[] args) throws IOException {
@@ -32,12 +42,14 @@ class Main {
 
         int answer = 2000000000;
         int ansIndex1 = -1, ansIndex2 = -1;
-        for(int i = 0; i < N; i++) {
-            ansIndex1 = i;
-            ansIndex2 = isExist(arr, i + 1, arr[i]);
-            int property = Math.abs(arr[ansIndex1] + arr[ansIndex2]);
+        for(int i = 0; i < N - 1; i++) {
+            int idx2 = isExist(arr, i + 1, arr[i]);
+            int property = Math.abs(arr[i] + arr[idx2]);
+
             if(property < answer) {
                 answer = property;
+                ansIndex1 = i;
+                ansIndex2 = idx2;
             }
         }
 
