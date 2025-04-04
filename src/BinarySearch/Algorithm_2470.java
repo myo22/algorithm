55b082cdef30dc726be7a223c0a37fb3d7f218e6
myo22 <dlalsgud12@naver.com>
2025-04-04 18:05:23 +0900

    [2470] 두 용액

--- src/BinarySearch/Algorithm_2470.java
+++ src/BinarySearch/Algorithm_2470.java
@@ -4,19 +4,18 @@ import java.util.*;
 import java.io.*;
 
 public class Algorithm_2470 {
-    public static int isExist(int[] arr, int i, int k) {
-        int l = i, r = arr.length - 1;
-        int closestIndex = i;
-        int minDiff = Integer.MAX_VALUE;
+    static int findOptimalPair(int[] arr, int fromIndex, int toIndex ,int value) {
+        int l = fromIndex, r = toIndex;
+        int optimalPairValue = arr[fromIndex];
+        int optimalAbs = 2000000000;
 
         while(l <= r) {
             int m = (l + r) / 2;
-            int sum = arr[m] + k;
+            int sum = arr[m] + value;
 
-            // 최소 절댓값 비교
-            if (Math.abs(sum) < minDiff) {
-                minDiff = Math.abs(sum);
-                closestIndex = m;
+            if (Math.abs(sum) < optimalAbs) {
+                optimalPairValue = arr[m];
+                optimalAbs = Math.abs(sum);
             }
 
             if(sum < 0) {
@@ -27,7 +26,7 @@ public class Algorithm_2470 {
                 return m;
             }
         }
-        return closestIndex;
+        return optimalPairValue;
     }
 
     public static void main(String[] args) throws IOException {
@@ -42,21 +41,21 @@ public class Algorithm_2470 {
 
         Arrays.sort(arr);
 
-        int answer = 2000000000;
-        int ansIndex1 = -1, ansIndex2 = -1;
+        int ansAbs = Math.abs(arr[0] + arr[1]);
+        int ans1 = arr[0], ans2 = arr[1];
         for(int i = 0; i < N - 1; i++) {
-            int idx2 = isExist(arr, i + 1, arr[i]);
-            int property = Math.abs(arr[i] + arr[idx2]);
+            int pairValue = findOptimalPair(arr, i + 1, N - 1, arr[i]);
+            int sumAbs = Math.abs(arr[i] + pairValue);
 
-            if(property < answer) {
-                answer = property;
-                ansIndex1 = i;
-                ansIndex2 = idx2;
+            if(sumAbs < ansAbs) {
+                ansAbs = sumAbs;
+                ans1 = arr[i];
+                ans2 = pairValue;
             }
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(arr[ansIndex1] + " " + arr[ansIndex2]);
+        bw.write(ans1 + " " + ans2);
         bw.flush();
     }
 }
