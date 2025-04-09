20fec4087a3a1b51bc6ef53d7b064be1f67b6153
myo22 <dlalsgud12@naver.com>
2025-04-09 18:24:20 +0900

    [2417] 정수 제곱근

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,41 @@ class Main {
         return q * q >= N;
     }
 
+    static long calcIntegerSqrt(long x) {
+        if(x == 0){
+            return 0;
+        }
+
+        long l = 1, r = 1L << 32, sqrt = -1;
+        while(l <= r) {
+            long m = (l + r) / 2;
+            if (isIntegerSqrt(x, m)) {
+                r = m - 1;
+                sqrt = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return sqrt;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
-        long q = (long) Math.sqrt(n);
 
-        if(isIntegerSqrt(n, q)) {
-            System.out.println(q);
-        }else{
-            q += 1;
-            System.out.println(q);
-        }
+        System.out.println(calcIntegerSqrt(n));
     }
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        long n = sc.nextLong();
+//        long q = (long) Math.sqrt(n);
+//
+//        if(isIntegerSqrt(n, q)) {
+//            System.out.println(q);
+//        }else{
+//            q += 1;
+//            System.out.println(q);
+//        }
+//    }
 }
