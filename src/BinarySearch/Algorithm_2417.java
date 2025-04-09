f480cc1f04d4d3aa5d50c688cc501a5393f8a11d
myo22 <dlalsgud12@naver.com>
2025-04-09 17:33:21 +0900

    [2417] 정수 제곱근

--- src/BinarySearch/Algorithm_2417.java
+++ src/BinarySearch/Algorithm_2417.java
@@ -3,12 +3,16 @@ package BinarySearch;
 import java.util.*;
 
 public class Algorithm_2417 {
+    static boolean isIntegerSqrt(long N, long q) {
+        return q * q >= N;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
         long q = (long) Math.sqrt(n);
 
-        if(q * q >= n) {
+        if(isIntegerSqrt(n, q)) {
             System.out.println(q);
         }else{
             q += 1;
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static boolean isIntegerSqrt(long N, long q) {
+        return q * q >= N;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
         long q = (long) Math.sqrt(n);
 
-        if(q * q >= n) {
+        if(isIntegerSqrt(n, q)) {
             System.out.println(q);
         }else{
             q += 1;
