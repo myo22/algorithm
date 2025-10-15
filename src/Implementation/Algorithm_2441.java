77d39190556d2ff5980d49173a23ddc259ebc70a
Jake <rlaalsgud0125@gmail.com>
2025-10-15 15:20:48 +0900

    [2441] 별 찍기 - 4

--- src/Implementation/Algorithm_2441.java
+++ src/Implementation/Algorithm_2441.java
@@ -1,9 +1,7 @@
 package Implementation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Algorithm_2441 {
     static FastReader scan = new FastReader();
