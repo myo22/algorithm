bb093d4011fa47b0cbc55430b7d9f33df51a627f
Jake <rlaalsgud0125@gmail.com>
2025-10-15 16:03:17 +0900

    [2445] 별 찍기 - 8

--- src/Implementation/Algorithm_2445.java
+++ src/Implementation/Algorithm_2445.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package Implementation;
 
-class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Algorithm_2445 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
