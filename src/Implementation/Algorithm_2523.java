02a4a8075f0b0f39212cf084ecbc2ec9d56385d0
Jake <rlaalsgud0125@gmail.com>
2025-10-17 13:12:32 +0900

    [2523] 별 찍기 - 13

--- src/Implementation/Algorithm_2523.java
+++ src/Implementation/Algorithm_2523.java
@@ -1,9 +1,11 @@
-import Implementation.Algorithm_2523;
+package Implementation;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
+public class Algorithm_2523 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
