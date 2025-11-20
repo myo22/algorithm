5ff5c8c8d8d21cd575b31f955a7fc0105504339b
Jake <rlaalsgud0125@gmail.com>
2025-11-20 22:26:26 +0900

    [10816] 숫자 카드 2 - 복습

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
@@ -9,48 +10,53 @@ public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N;
-    static ArrayList<Integer>[] arr;
+    static int N, M;
+    static int[] card;
 
     static void input() {
         N = scan.nextInt();
-        arr = new ArrayList[N + 1];
+        card = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            arr[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= N; i++) {
-            int coord = scan.nextInt();
-            int color = scan.nextInt();
-            arr[color].add(coord);
+            card[i] = scan.nextInt();
         }
     }
 
-    static int toLeft(int color, int idx) {
-        if (idx == 0) {
-            return Integer.MAX_VALUE;
+    static int lower_bound(int L, int R, int X) {
+        int ans = R + 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (card[mid] >= X) {
+               ans = mid;
+               R = mid - 1;
+            } else {
+               L = mid + 1;
+            }
         }
-        return arr[color].get(idx) - arr[color].get(idx - 1);
+        return ans;
     }
 
-    static int toRight(int color, int idx) {
-        if (idx + 1 == arr[color].size()) {
-            return Integer.MAX_VALUE;
+    static int upper_bound(int L, int R, int X) {
+        int ans = R + 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (card[mid] > X) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
         }
-        return arr[color].get(idx + 1) - arr[color].get(idx);
+        return ans;
     }
 
     static void pro() {
-        for (int color = 1; color <= N; color++) {
-            Collections.sort(arr[color]);
+        M = scan.nextInt();
+        Arrays.sort(card, 1, N + 1);
+        for (int i = 1; i <= M; i++) {
+            int X = scan.nextInt();
+            sb.append(upper_bound(1, N, X) - lower_bound(1, N, X)).append(" ");
         }
-
-        int sum = 0;
-        for(int color = 1; color <= N; color++) {
-            for(int coord = 0; coord < arr[color].size(); coord++) {
-                sum += Math.min(toLeft(color, coord), toRight(color, coord));
-            }
-        }
-        System.out.println(sum);
+        System.out.println(sb.toString());
     }
 
 
