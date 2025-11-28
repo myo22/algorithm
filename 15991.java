275775eba452c9e61477cf26921486f5527ad3ca
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2025-11-28 13:00:16 +0900

    [15991] 1, 2, 3 더하기 6
    https://www.acmicpc.net/problem/15991

--- 15991.java
+++ 15991.java
@@ -3,14 +3,29 @@ import java.io.IOException;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
+    static int T;
+    static int[] dp, A;
+
     static void input() {
+        T = scan.netxtInt();
+        dp = new int[N];
+        A = new int[N];
+        for (int i = 1; i <= T; i+) {
+            A[i] = scan.nextInt();
+        }
     }
 
     static void pro() {
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        for (int i = 4; i <= 100000; i++) {
+            dp[i] = (i - 2 > 0 ? dp[i-2] : 0) + (i - 4 > 0 ? dp[i-4] : 0) + (i - 6 > 0 ? dp[i-6] : 0);
+        }
     }
 
 
