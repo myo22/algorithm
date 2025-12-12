4f0cb90aa2e71c0f955e6aa19e141844f2deadbb
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2025-12-12 12:50:05 +0900

    [11052] 카드 구매하기
    https://www.acmicpc.net/problem/11052

---
+++ 11052.java
@@ -0,0 +1,79 @@
+import java.io.IOException;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] P, dy;
+
+    static void input() {
+        N = scan.nextInt();
+        P = new int[N + 1];
+        dy = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            P[i] = scan.nextInt();
+        }
+    }
+
+    static void pro() {
+        dy[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int cnt = 1; cnt <= i; cnt++) {
+                dy[i] = Math.max(P[i], P[i - cnt] + dy[cnt]);
+            }
+        }
+        System.out.println(dy[N]);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
