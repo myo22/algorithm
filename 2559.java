b5a23aa95b9c6d4a7c8c722d8b8a58f9e998ff15
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2025-12-23 08:43:37 +0900

    [2559] 수열
    https://www.acmicpc.net/problem/2559

---
+++ 2559.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, K, sum, max;
+    static int[] arr;
+
+    static void input() {
+        N = scan.nextInt();
+        K = scan.nextInt();
+        arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = scan.nextInt();
+        }
+    }
+
+    static void pro() {
+        for (int i = 1; i <= K; i++) {
+            sum += arr[i];
+        }
+        
+        max = Integer.MIN_VALUE;
+        for (int i = K + 1; i <= N; i++) {
+            sum += arr[i];
+            sum -= arr[i - K];
+        
+            max = Math.max(sum, max);
+        }
+        System.out.println(max);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
