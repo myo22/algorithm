2d02b9f2192b93241fe69315288aa50dab8249e7
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2025-12-29 08:51:33 +0900

    [14888] 연산자 끼워넣기
    https://www.acmicpc.net/problem/14888

--- src/BruteForce/14888.java
+++ src/BruteForce/14888.java
@@ -7,46 +7,45 @@ class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, max, min;
-    static int[] A, order;
-    static int[] operator;
+    static int N, min, max;
+    static int[] Operator, A;
 
     static void input() {
         N = scan.nextInt();
         A = new int[N + 1];
-        operator = new int[5];
+        Operator = new int[5];
         for (int i = 1; i <= N; i++) {
             A[i] = scan.nextInt();
         }
         for (int i = 1; i <= 4; i++) {
-            operator[i] = scan.nextInt();
+            Operator[i] = scan.nextInt();
         }
-        max = Integer.MIN_VALUE;
         min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
     }
 
-    static int calculator(int operation1, int operator, int operation2) {
-        if (operator == 1) {
-            return operation1 + operation2;
-        } else if (operator == 2) {
-            return operation1 - operation2;
-        } else if (operator == 3) {
-            return operation1 * operation2;
+    static int calculator(int a, int b, int k) {
+        if (k == 1) {
+            return a + b;
+        } else if (k == 2) {
+            return a - b;
+        } else if (k == 3) {
+            return a * b;
         } else {
-            return operation1 / operation2;
+            return a / b;
         }
     }
 
     static void rec_func(int k, int value) {
-        if (N == k) {
-            max = Math.max(max, value);
-            min = Math.min(min, value);
+        if (k == N) {
+            max = Math.max(value, max);
+            min = Math.min(value, min);
         } else {
             for (int i = 1; i <= 4; i++) {
-                if (operator[i] > 0) {
-                    operator[i]--;
-                    rec_func(k + 1, calculator(value, i, A[k + 1]));
-                    operator[i]++;
+                if (Operator[i] > 0) {
+                    Operator[i]--;
+                    rec_func(k + 1, calculator(value, A[k + 1], i));
+                    Operator[i]++;
                 }
             }
         }
@@ -56,8 +55,9 @@ class Main {
     public static void main(String[] args) throws IOException {
         input();
         rec_func(1, A[1]);
-        sb.append(max).append("\n").append(min);
-        System.out.println(sb.toString());
+
+        System.out.println(max);
+        System.out.println(min);
     }
 
     static class FastReader {
