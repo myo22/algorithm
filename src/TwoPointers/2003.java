78d86eff52f0ff8db958f5e65293eaad68bd1de5
이민형 <rlaalsgud0125@gmail.com>
2026-03-04 23:42:21 +0900

    [2003] 수들의 합 2
    https://www.acmicpc.net/problem/2003

--- src/TwoPointers/2003.java
+++ src/TwoPointers/2003.java
@@ -1,43 +1,40 @@
-package TwoPointers;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Algorithm_2003 {
+import java.io.*;
+import java.util.*;
+
+class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static int N, M;
-    static int[] A;
+    static int[] arr;
 
     static void input() {
         N = scan.nextInt();
         M = scan.nextInt();
-        A = new int[N + 1];
+        arr = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
+            arr[i] = scan.nextInt();
         }
     }
-
 
     static void pro() {
-        int right = 1, left = 1, cnt = 0, sum = 0;
-        while (true) {
-            if (sum >= M) {
-                if (sum == M) cnt++;
-                sum -= A[left++];
-            } else {
-                if (right > N) break;
-                sum += A[right++];
+        int sum = 0; int R = 0; int count = 0;
+        for (int L = 1; L <= N; L++) {
+            sum -= arr[L - 1];
+            while (R + 1 <= N && sum < M) {
+                sum += arr[++R];
+            }
+            if (sum == M) {
+                count++;
             }
         }
-        System.out.println(cnt);
+        System.out.println(count);
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         input();
         pro();
     }
