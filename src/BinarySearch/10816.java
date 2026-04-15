eae03024e50a61ca545fcf8c34e5e4697bb3b9ed
이민형 <dlalsgud12@naver.com>
2026-04-15 09:23:07 +0900

    [10816] 숫자 카드 2
    https://www.acmicpc.net/problem/10816

--- src/BinarySearch/10816.java
+++ src/BinarySearch/10816.java
@@ -8,53 +8,55 @@ class Main {
     static StringBuilder sb = new StringBuilder();
 
     static int N, M;
-    static int[] card;
+    static int[] A;
 
     static void input() {
         N = scan.nextInt();
-        card = new int[N + 1];
+        A = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            card[i] = scan.nextInt();
+            A[i] = scan.nextInt();
         }
-        M = scan.nextInt();
     }
 
-    static int lower_bound(int L, int R, int k) {
-        int ans = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (card[mid] >= k) {
+    static int lowerBound(int k) {
+        int left = 1, right = N;
+        int ans = N + 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (A[mid] >= k) {
                 ans = mid;
-                R = mid - 1;
+                right = mid - 1;
             } else {
-                L = mid + 1;
+                left = mid + 1;
             }
         }
         return ans;
     }
 
-    static int upper_bound(int L, int R, int k) {
-        int ans = R + 1;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (card[mid] > k) {
+    static int upperBound(int k) {
+        int left = 1, right = N;
+        int ans = N + 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if (A[mid] > k) {
                 ans = mid;
-                R = mid - 1;
+                right = mid - 1;
             } else {
-                L = mid + 1;
+                left = mid + 1;
             }
         }
         return ans;
     }
+
     static void pro() {
-        Arrays.sort(card, 1, N + 1);
+        Arrays.sort(A, 1, N + 1);
+        int M = scan.nextInt();
         for (int i = 1; i <= M; i++) {
             int k = scan.nextInt();
-            int upper = upper_bound(1, N, k);
-            int lower = lower_bound(1, N, k);
-            sb.append(upper - lower).append(' ');
+            sb.append(upperBound(k) - lowerBound(k)).append(" ");
         }
-        System.out.println(sb.toString());
+
+        System.out.print(sb.toString());
     }
 
 
