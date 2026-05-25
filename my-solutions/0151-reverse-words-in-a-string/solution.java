2fb29011e4c9230f89e996d4b72e018ea9dfbc5c
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-05-25 22:35:28 +0000

    Sync LeetCode submission Runtime - 9 ms (23.69%), Memory - 47.8 MB (5.21%)

---
+++ my-solutions/0151-reverse-words-in-a-string/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().replaceAll(" +", " ").split(" ");
+        String answer = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            answer += arr[i];
+            if (i != 0) {
+                answer += " ";
+            }
+                  }
+                  return answer;
+    }
+}
