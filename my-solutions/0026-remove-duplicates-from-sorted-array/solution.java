0afc04d5c704cd317ccfec546ae24ffb5ccdd968
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-03 22:26:24 +0000

    Sync LeetCode submission Runtime - 1 ms (76.68%), Memory - 46.3 MB (94.86%)

--- my-solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ my-solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[k++] = nums[i];
+        int k = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                nums[++k] = nums[i + 1];
             }
         }
-        return k;
+        return k + 1;
     }
 }
