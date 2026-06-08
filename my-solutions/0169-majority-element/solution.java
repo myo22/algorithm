f0496e007629310d319f0ab5618d30acd274573d
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-08 22:50:52 +0000

    Sync LeetCode submission Runtime - 1 ms (99.89%), Memory - 55.7 MB (30.83%)

--- my-solutions/0169-majority-element/solution.java
+++ my-solutions/0169-majority-element/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = 0;
+        int current = 0;
         int count = 0;
-        
-        for (int num : nums) {
-            // 카운트가 0이면 새로운 후보를 선정
+        for (int i = 0; i < nums.length; i++) {
             if (count == 0) {
-                candidate = num;
+                current = nums[i];
             }
             
-            // 후보와 같으면 count 증가, 다르면 감소
-            count += (num == candidate) ? 1 : -1;
+            if (current == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }  
         }
-        
-        return candidate;
+        return current;
     }
 }
