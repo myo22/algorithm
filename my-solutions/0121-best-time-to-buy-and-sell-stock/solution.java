1105570f0403afd9a0262a78ff80c73f6fd7550c
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-11 22:21:28 +0000

    Sync LeetCode submission Runtime - 2 ms (41.24%), Memory - 94.5 MB (31.69%)

--- my-solutions/0121-best-time-to-buy-and-sell-stock/solution.java
+++ my-solutions/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minprice = prices[0];
-        int profit = 0;
+        int curPrice = prices[0];
+        int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < minprice) {
-                minprice = prices[i];
-            } else if (prices[i] - minprice > profit) {
-                profit = prices[i] - minprice;
+            if (prices[i] < curPrice) {
+                curPrice = prices[i];
+            } else if (maxProfit < prices[i] - curPrice) {
+                maxProfit = prices[i] - curPrice;
             }
         }
-        return profit;
+        return maxProfit;
     }
 }
