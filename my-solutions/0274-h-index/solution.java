4789f4d6d2011b88da647672f2e64bd381d01a50
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-16 22:54:42 +0000

    Sync LeetCode submission Runtime - 5 ms (68.27%), Memory - 43.5 MB (41.90%)

--- my-solutions/0274-h-index/solution.java
+++ my-solutions/0274-h-index/solution.java
@@ -1,13 +1,21 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n = citations.length;
         Arrays.sort(citations);
+        
+        int n = citations.length;
+        int h = 0;
+        
         for (int i = 0; i < n; i++) {
-            int h = n - i;
-            if (citations[i] >= h) {
-                return h;
-            }     
+            int val = citations[n - 1 - i];
+            
+            // h+1편의 논문이 최소 h+1번 인용되었는지 확인
+            if (val >= i + 1) {
+                h = i + 1;
+            } else {
+                break;
+            }
         }
-        return 0;
+        return h;
     }
 }
+
