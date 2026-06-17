91f44a1a2f2af8d39ac37ebf39f1fbbf3d95e0b7
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-17 22:45:35 +0000

    Sync LeetCode submission Runtime - 205 ms (5.08%), Memory - 100.5 MB (63.79%)

--- my-solutions/0380-insert-delete-getrandom-o1/solution.java
+++ my-solutions/0380-insert-delete-getrandom-o1/solution.java
@@ -1,43 +1,33 @@
 class RandomizedSet {
     
     private List<Integer> nums;
-    private Map<Integer, Integer> locs;
     private Random rand;
 
     public RandomizedSet() {
         nums = new ArrayList();
-        locs = new HashMap<>();
         rand = new Random();
     }
     
     public boolean insert(int val) {
-        if (locs.containsKey(val)) {
+        if (nums.contains(val)) {
             return false;
         }
-        locs.put(val, nums.size());
         nums.add(val);
         return true;
     }
     
     public boolean remove(int val) {
-        if (!locs.containsKey(val)) {
+        if (!nums.contains(val)) {
             return false;
-        }
-        int idx = locs.get(val);
-        int lastVal = nums.get(nums.size() - 1);
-        
-        nums.set(idx, lastVal);
-        locs.put(lastVal, idx);
-        
-        nums.remove(nums.size() - 1);
-        locs.remove(val);
+        }    
+        nums.remove(Integer.valueOf(val));
         return true;
     }
     
     public int getRandom() {
-        int randomIndex = rand.nextInt(nums.size());
+        int rIndex = rand.nextInt(nums.size());
         
-        return nums.get(randomIndex);
+        return nums.get(rIndex);
     }
 }
 
