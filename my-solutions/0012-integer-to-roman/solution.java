79d347a1d1079a2073e152cdbf4e7d5963e42885
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-22 22:38:21 +0000

    Sync LeetCode submission Runtime - 4 ms (31.55%), Memory - 46.2 MB (89.50%)

--- my-solutions/0012-integer-to-roman/solution.java
+++ my-solutions/0012-integer-to-roman/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols =  {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                sb.append(symbols[i]);
+        int[] symbol = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] value = {"M", "CM", "D","CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        String answer = "";     
+        for (int i = 0; i < symbol.length; i++) {
+            int cnt = num / symbol[i];
+            num %= symbol[i];
+            for (int j = 0; j < cnt; j++) {
+                answer += value[i];
             }
         }
-        return sb.toString();
+        return answer;
     }
 }
