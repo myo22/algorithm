a6e1112a6ddadbccd3e583725c5f53fdc54ef4e1
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-06-23 22:35:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (6.63%)

--- my-solutions/0058-length-of-last-word/solution.java
+++ my-solutions/0058-length-of-last-word/solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count = 0;
-        boolean isChar = false;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
-                isChar = true;
-            }
-            if (isChar && s.charAt(i) == ' ') {
-                break;
-            }
-            if (isChar) {
-                count++;
-            }
-        }        
+        int idx = s.length() - 1;
+        while (idx >= 0 && s.charAt(idx) == ' ') {
+            idx--;
+        }
+        
+        while (idx >= 0 && s.charAt(idx) != ' ') {
+            count++;
+            idx--;
+        }
+        
         return count;
     }
 }
