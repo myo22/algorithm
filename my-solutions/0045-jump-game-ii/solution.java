8fe523e386914394e0067264de7ec509c3405fa2
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-08-05 22:24:08 +0000

    Sync LeetCode submission Runtime - 1 ms (99.73%), Memory - 46.9 MB (97.15%)

--- my-solutions/0045-jump-game-ii/solution.java
+++ my-solutions/0045-jump-game-ii/solution.java
@@ -1,18 +1,20 @@
 class Solution {
     public int jump(int[] nums) {
+        int n = nums.length;
+        int maxJump = 0;
         int curEnd = 0;
-        int fartest = 0;
         int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            fartest = Math.max(fartest, i + nums[i]);
+        for (int i = 0; i < n - 1; i++) {
+            maxJump = Math.max(maxJump, i + nums[i]);
             if (i == curEnd) {
-                curEnd = fartest;
-                count++;
-                
-                if (curEnd >= nums.length - 1) {
-                    break;        
-                }                 
+               curEnd = maxJump;  
+               count++;
+               
+               if (curEnd >= n - 1) {
+                  break;
+               }     
             }           
+            
         }
         return count;
     }
